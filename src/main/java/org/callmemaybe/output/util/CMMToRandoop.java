f61ca7af48f447ae2aacc4b26427538e3c40bdd2
Arianna Blasi <blasia@usi.ch>
2022-02-21 14:24:57 +0100

    Changing what we put into Randoop specs

--- src/main/java/org/callmemaybe/output/util/CMMToRandoop.java
+++ src/main/java/org/callmemaybe/output/util/CMMToRandoop.java
@@ -1,24 +1,28 @@
 package org.callmemaybe.output.util;
 
-import org.callmemaybe.extractor.DocumentedExecutable;
-
 public class CMMToRandoop {
-    public Type containingClass;
-    public String operationSignature;
-    public String mustPrecede;
-    public String mustFollow;
+  //    public Type containingClass;
+  public String operationSignature;
+  public boolean isConstructor;
+  public String mustPrecede;
+  public String mustFollow;
+  public String comment;
 
-    public CMMToRandoop(DocumentedExecutable member,
-                        String operationSignature,
-                        String mustPrecede,
-                        String mustFollow) {
-        this.operationSignature = operationSignature;
-        this.mustPrecede = mustPrecede;
-        this.mustFollow = mustFollow;
-        this.containingClass =
-                new org.callmemaybe.output.util.Type(
-                        member.getDeclaringClass().getName(),
-                        member.getDeclaringClass().getSimpleName(),
-                        member.getDeclaringClass().isArray());
-    }
+  public CMMToRandoop(
+      String operationSignature,
+      boolean isConstructor,
+      String mustPrecede,
+      String mustFollow,
+      String comment) {
+    this.operationSignature = operationSignature;
+    this.isConstructor = isConstructor;
+    this.mustPrecede = mustPrecede;
+    this.mustFollow = mustFollow;
+    this.comment = comment;
+    //        this.containingClass =
+    //                new org.callmemaybe.output.util.Type(
+    //                        member.getDeclaringClass().getName(),
+    //                        member.getDeclaringClass().getSimpleName(),
+    //                        member.getDeclaringClass().isArray());
+  }
 }
--- src/main/java/org/callmemaybe/output/util/CMMToRandoopSerializer.java
+++ src/main/java/org/callmemaybe/output/util/CMMToRandoopSerializer.java
@@ -4,13 +4,12 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-
 import java.lang.reflect.Type;
 
 public class CMMToRandoopSerializer implements JsonSerializer<CMMToRandoop> {
 
-    @Override
-    public JsonElement serialize(CMMToRandoop src, Type typeOfSrc, JsonSerializationContext context) {
-        return new GsonBuilder().create().toJsonTree(src);
-    }
+  @Override
+  public JsonElement serialize(CMMToRandoop src, Type typeOfSrc, JsonSerializationContext context) {
+    return new GsonBuilder().create().toJsonTree(src);
+  }
 }
